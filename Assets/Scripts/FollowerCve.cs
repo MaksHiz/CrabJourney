34f91124630d0949d94d6492c7ac6bcb9ef62d98
PetraTurkovic <petra.turkovic@fer.unizg.hr>
2024-12-14 16:25:55 +0100

    New BACKGROUND i novi CAMERA movement

---
+++ Assets/Scripts/FollowerCve.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowerCve : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float followspeed = 0.6f;
+    public Transform target;
+    public Vector3 offset = new Vector3(-2f, -1f, 0f); 
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 newPOs = new Vector3(target.position.x, target.position.y, -10f)+ offset;
+        transform.position = Vector3.Slerp(transform.position, newPOs, followspeed*Time.deltaTime);
+    }
+}

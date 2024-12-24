9be0f6744b2ad656dddb4c65c1637d41ca6ee625
PetraTurkovic <petra.turkovic@fer.unizg.hr>
2024-12-25 00:44:42 +0100

    Brate dosta commitova vise

--- Assets/Scripts/ShowTutorial.cs
+++ Assets/Scripts/ShowTutorial.cs
@@ -8,19 +8,14 @@ public class ShowTutorial : MonoBehaviour
 
     void Start(){
         bubbletext.SetActive(false);
-        if (bubbletext == null)
-    {
-        Debug.LogError("Bubbletext GameObject is not assigned in the Inspector!");
-        return;
     }
-    }
-    private void OnTriggerEnter(Collider other){
+    private void OnTriggerEnter2D(Collider2D other){
         if (other.CompareTag("Player")){
             bubbletext.SetActive(true);
         }
         Debug.Log("Omg");
     }
-    private void OnTriggerExit(Collider other){
+    private void OnTriggerExit2D(Collider2D other){
         if (other.CompareTag("Player")){
             bubbletext.SetActive(false);
         }

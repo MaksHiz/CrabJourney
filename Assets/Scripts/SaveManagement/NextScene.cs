ebdf9fc26bae27cb22e462dd70aab36ad001cca4
MaksHiz <maksimilijan.hizman@gmail.com>
2025-01-29 17:07:09 +0100

    Added Next Scene Trigger Logic
    might have conflicts

--- Assets/Scripts/SaveManagement/NextScene.cs
+++ Assets/Scripts/SaveManagement/NextScene.cs
@@ -6,18 +6,25 @@ using UnityEngine.SceneManagement;
 public class NextScene : MonoBehaviour
 {
     public Animator animator;
-    public string scene;
+    public string nextScene;
+    public string currentScene;
     void OnTriggerEnter2D(Collider2D other){
          if (other.CompareTag("Player")){
-            GameSave.CurrentSave.LoadCrabToPosition = false;
-            GameSave.CurrentSave.CrabPositionScene = scene;
+            GameSave.CurrentSave.LoadCrabToPosition = true;
+            GameSave.CurrentSave.CrabPositionScene = nextScene;
             AudioManager.Instance.PlaySFX("jump_into_water");
             animator.SetTrigger("FadOut");
             StartCoroutine(WaitForAnimationAndChangeScene());
+            if (currentScene == "Beach" && nextScene == "Level1") { GameSave.CurrentSave.CrabPosition = new Vector3((float)-20.1800003, (float)75.6600037, 0); }
+            if (currentScene == "Level1" && nextScene == "Beach" ) { GameSave.CurrentSave.CrabPosition = new Vector3((float)97.8000031, (float)-3.16000009, 0); }
+            if (currentScene == "Level1" && nextScene == "Cave1") { GameSave.CurrentSave.CrabPosition = new Vector3((float)68.4300003, (float)-10.3400002, 0); }
+            if (currentScene == "Cave1" && nextScene == "Level1") { GameSave.CurrentSave.CrabPosition = new Vector3((float)-14.04, (float)23.6599998, 0); }
+            if (currentScene == "Cave1" && nextScene == "DeepWater") { GameSave.CurrentSave.CrabPosition = new Vector3((float)-25.0400009, (float)-0.230387881, 0); }
+            if (currentScene == "DeepWater" && nextScene == "Cave1") { GameSave.CurrentSave.CrabPosition = new Vector3((float)28.1900005, (float)-10.3400002, 0);  }
          }
     }
     private IEnumerator WaitForAnimationAndChangeScene(){
          yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
-         SceneManager.LoadScene(scene);
+         SceneManager.LoadScene(nextScene);
     }
 }

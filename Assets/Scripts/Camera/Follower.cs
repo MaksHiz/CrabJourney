214a678d9305ee8236a9792a5cc88cf2772656ad
vito-vrbic <vito.vrbic@fer.unizg.hr>
2025-01-30 12:23:39 +0100

    dud

--- Assets/Scripts/Camera/Follower.cs
+++ Assets/Scripts/Camera/Follower.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Follower : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public float followspeed = 1f;
+    public float followSpeedX = 1f; // Speed for X-axis
+    public float followSpeedY = 3f; // Faster speed for Y-axis
     public Transform target;
     public Vector3 offset = new Vector3(2f, 1f, 0f);
 
@@ -14,10 +14,15 @@ public class Follower : MonoBehaviour
         transform.position = target.position + offset;
     }
 
-    // Update is called once per frame
     void LateUpdate()
     {
-        Vector3 newPOs = new Vector3(target.position.x, target.position.y, -10f)+ offset;
-        transform.position = Vector3.Slerp(transform.position, newPOs, followspeed*Time.deltaTime);
+        Vector3 targetPos = target.position + offset;
+        Vector3 newPos = new Vector3(
+            Mathf.Lerp(transform.position.x, targetPos.x, followSpeedX * Time.deltaTime), // Smooth follow on X
+            Mathf.Lerp(transform.position.y, targetPos.y, followSpeedY * Time.deltaTime), // Faster follow on Y
+            -10f // Fixed Z-position
+        );
+
+        transform.position = newPos;
     }
 }
